6db203f20888eccb7f7dceea14c2d0753273e22a
mastersprinkler112 <lorenzopogi014@gmail.com>
2025-06-15 00:54:49 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 義 𝗚𝗶𝘆𝘂 | 🌊 勇 ]";
+const doNotDelete = "[ 𝙍𝘾𝘾 𝘽𝙊𝙏 𝙑𝟯 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
